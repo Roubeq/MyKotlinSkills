e9b8c09e04a53e4231f1d4fd1b1a02caf2d11b37
Roubeq <arteem.blinov.2005@gmail.com>
2025-06-13 14:00:38 +0300

    Foreground Service(all works)

--- ForegroundService/app/build.gradle.kts
+++ ForegroundService/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.foregroundservice"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.foregroundservice"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
